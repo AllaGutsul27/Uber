4f1ce1457813650eac183f4e60391ecbfa4f3788
AllaGutsul27 <111051621+AllaGutsul27@users.noreply.github.com>
2022-11-16 15:23:56 +0200

    Added all files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('.menu');
+let menuItem = document.querySelectorAll('.menu_item');
+let hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    })
+})
